b76a96aa7268f2e60312aa5861ef7d2a95ceee3b
Claire Schults <claire.schults@gmail.com>
2017-05-16 12:33:53 +0200

    Rails Task Manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "Pick-up laundry", description: "Go Pick-up by Friday")
+Task.create(name: "Groceries", description: "Get tea and shampoo")
+Task.create(name: "Call Grandma", description: "06 61 36 23 12")
+Task.create(name: "Pay Le Wagon", description: "Third Payment, do by end of week")
